af6253ecd2417bdfaf29e899abf7320f700fb25e
ElizavetaPonkratova <elizaveta.ski@yandex.ru>
2022-12-06 21:29:15 +0700

    HWSem1-1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Например:
+456 -> 5 782 -> 8 918 -> 1*/
+
+Random random = new Random();
+int randomNumber = random.Next(99, 1000);
+
+Console.WriteLine(randomNumber);
+Console.WriteLine(randomNumber / 10 % 10);
