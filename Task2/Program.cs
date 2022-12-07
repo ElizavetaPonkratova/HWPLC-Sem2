657d7d98e18ed678b4e45def1c792a6c11582269
ElizavetaPonkratova <elizaveta.ski@yandex.ru>
2022-12-07 21:47:13 +0700

    Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Например:
+645 -> 5 78 -> третьей цифры нет 32679 -> 6*/
+
+int result = 0;
+Console.WriteLine("Enter number:");
+int number = int.Parse(Console.ReadLine());
+              
+
+if (number >= 100)
+{
+    while (number > 999)
+    {
+        number = number /10;
+    }
+    result = number % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("third number is missing");
+}
