1deda6d7bd7e64541bfb8f7c11ba52ce583ad35a
ElizavetaPonkratova <elizaveta.ski@yandex.ru>
2022-12-07 22:09:28 +0700

    HWSem2-3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Например: 6 -> да 7 -> да 1 -> нет*/
+
+Console.WriteLine("Enter number day week:");
+int numberDay = int.Parse(Console.ReadLine());
+
+if ((numberDay >0) && (numberDay < 8))
+{
+    if (numberDay >= 6)
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
+else
+{
+    Console.WriteLine("There is no such day of the week!");
+}
